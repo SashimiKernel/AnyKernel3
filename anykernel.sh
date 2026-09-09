12a0741c93c3765000d54307baffae444b8e8b38
Mr. M <gabisantanapereira517@gmail.com>
2026-09-08 21:21:09 -0300

    ak3: remove username

--- anykernel.sh
+++ anykernel.sh
@@ -1,14 +1,14 @@
 ### AnyKernel3 Ramdisk Mod Script
 
 properties() { '
-kernel.string=Sashimi Kernel by @MrEliteBall
+kernel.string=Sashimi
 do.devicecheck=1
 do.modules=0
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=bangkk
-supported.versions=16-17
+supported.versions=16.1-17
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; }
